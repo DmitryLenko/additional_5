fe8f9bde8584e7b6c4d7b0ba78b1fd6b9683c67e
DmitryLenko <dias1381254@gmail.com>
2018-09-12 19:51:31 +0300

    adding additional verification

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   var stack = [];
-   var arr = [];
 
 for(var i = 0; i < str.length; i++) {
 	for(var j = 0; j < bracketsConfig.length; j++) {
-		if(str[i] === bracketsConfig[j][0]) {
+		if(str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] != bracketsConfig[j][1]) {
       stack.push(str[i]);
-		} else if(str[i] === bracketsConfig[j][1]) {
+		} else if(str[i] === bracketsConfig[j][1] && bracketsConfig[j][0] != bracketsConfig[j][1]) {
       var last = stack.pop();
 			if(last === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
 			} else {
 				return false
 			} 
 		}
-  }
 }
   if (stack.length !== 0) {
     return false;
