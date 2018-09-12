19754a0f9dc43ab57c613ec2e93bb48ae893ce95
DmitryLenko <dias1381254@gmail.com>
2018-09-12 19:49:04 +0300

    adding stack check, identical brackets not checked

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack = [];
+   var arr = [];
+
+for(var i = 0; i < str.length; i++) {
+	for(var j = 0; j < bracketsConfig.length; j++) {
+		if(str[i] === bracketsConfig[j][0]) {
+      stack.push(str[i]);
+		} else if(str[i] === bracketsConfig[j][1]) {
+      var last = stack.pop();
+			if(last === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
+			} else {
+				return false
+			} 
+		}
+  }
+}
+  if (stack.length !== 0) {
+    return false;
+  } {
+    return true;
+  }
+
 }
